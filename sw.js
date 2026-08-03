1d7e32465fd7578f173aecbf1c9fbea94ebe9686
cecemeri48-beep <cecemeri48@gmail.com>
2026-08-04 01:26:47 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='reichas-v31';
+const CACHE='reichas-v33';
 const CORE=['/','/index.html','/manifest.webmanifest','/icon-180.png','/icon-192.png','/icon-512.png','/icon-maskable-512.png','/og-image.jpg','/logo.png','/hero.jpg','/paper.jpg'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(CORE)}).catch(function(){}));self.skipWaiting()});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.map(function(k){return k!==CACHE?caches.delete(k):null}))}));self.clients.claim()});
