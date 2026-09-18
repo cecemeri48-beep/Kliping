fdad54dbe84e8f2858c452201bf0f6bc5034c4f5
cecemeri48-beep <cecemeri48@gmail.com>
2026-09-18 22:39:00 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='reichas-v92-final-wowo';
+const CACHE='reichas-v98-buka-segera';
 /* Hanya aset yang benar-benar dipakai pada tampilan pertama. og-image.jpg
    (156 KB) dan ikon 512 px dibuang dari precache: keduanya tidak pernah
    ditampilkan ke pembaca (hanya untuk pratinjau WhatsApp dan ikon PWA),
